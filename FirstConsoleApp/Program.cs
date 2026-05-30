f3caea95fbe81c51a7afca04f35bd679b2bb2e3b
haripriya4452 <haripriyaashok4452@gmail.com>
2026-05-30 22:18:15 +0530

    Added employee details console example

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("=====Employee Details=====");
+string employee_name = "Hari";
+int employeeAge = 26;
+double salary = 45000;
+
+Console.WriteLine($"Employee Name :  {employee_name}" );
+Console.WriteLine($"Employee Age :  {employeeAge}" );
+Console.WriteLine($"Employee Salary :  {salary}" );
